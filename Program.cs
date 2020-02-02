e6f4a84d12b467db176925d0d564c5bd09514f63
ikor <igor_korenovsk@mail.ru>
2020-02-02 16:03:57 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            f4_1(2);
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+
+        static void f4_1(byte x)
+        {
+            var list = new Boolean[8];
+            for (int i = 0; i < 8; i++)
+            {             
+                list[i] = ((x & (1 << i))!=0 );
+            }
+            Array.Reverse(list);
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write(list[i]==false?0:1);
+            }      
+        }
+
+    }
+}
