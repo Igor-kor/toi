a4ade1110118f9168eac6301ff4238f0388ae33d
ikor <igor_korenovsk@mail.ru>
2020-02-02 17:07:19 +0300

    1

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@ namespace toi
     {
         static void Main(string[] args)
         {
-            f4_1(2);
+            //f4_1(2);
+            //f4_2(2);
+            //f4_4(256);
+            //Console.Write(f4_3(240,3));
+            //f5_4();
+            f5_5();
             Console.WriteLine();
             Console.ReadKey();
 
@@ -20,14 +25,65 @@ namespace toi
         {
             var list = new Boolean[8];
             for (int i = 0; i < 8; i++)
-            {             
-                list[i] = ((x & (1 << i))!=0 );
+            {
+                list[i] = ((x & (1 << i)) != 0);
             }
             Array.Reverse(list);
             for (int i = 0; i < 8; i++)
             {
-                Console.Write(list[i]==false?0:1);
-            }      
+                Console.Write(list[i] == false ? 0 : 1);
+            }
+        }
+        static void f4_2(byte x)
+        {
+            Console.Write((x & (1 << 3)) == (x & (1 << 4)) ? "Биты одинаковы" : "Биты не одинаковы");
+        }
+
+        static void f4_4(int x)
+        {
+            for (int i = 0, k = 7; i < 8; i++, k--)
+            {
+                if (Convert.ToBoolean(x & (1 << i)) != Convert.ToBoolean(x & (1 << k)))
+                {
+                    Console.Write("старшая и младшая тетрада не равны");
+                    return;
+                }
+            }
+            Console.Write("старшая и младшая тетрада равны");
+        }
+
+        // todo 
+        static byte f4_3(byte x, int setTetr)
+        {
+            return (byte)((x & 0b00001111) & (setTetr));
+        }
+
+
+        static void f5_4()
+        {
+            //sbyte x = -128,y = 8;
+            //sbyte x = -1, y = 64;
+            //short x = -129, y = 16; 
+            int x = 0x7fffffff, y = 16;  
+            //int x = 0x7fffffff, y = 32; 
+            //int x = 0x7fff0000, y = 8;  
+
+            var list = new Boolean[y];
+            for (int i = 0; i < y; i++)
+            {
+                list[i] = ((x & (1 << i)) != 0);
+            }
+            Array.Reverse(list);
+            for (int i = 0; i < y; i++)
+            {
+                Console.Write(list[i] == false ? 0 : 1);
+            }
+            Console.WriteLine();
+        }
+
+        static void f5_5()
+        {
+            Console.WriteLine("минимальное значение long = {0}, максимальное значение long = {1}", long.MinValue, long.MaxValue);
         }
 
     }
